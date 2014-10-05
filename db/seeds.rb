7d9ef16db8fb84b201796bd90ebeb45f02a5fbd6
rapoulson <rebecca.poulson@gmail.com>
2014-10-05 17:37:18 -0400

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-CSV.foreach("/Users/rebeccapoulson/Desktop/raines_specs.csv", headers: true) do |row|
+CSV.foreach("/home/devoe/raines_specs.csv", headers: true) do |row|
 
   d = Drink.create( :name => row["Name"], :method => row["Method"], :ice => row["Ice"],
    :garnish => row["Garnish"], :notes => row["Notes"],  :glass => row["Glass"])
