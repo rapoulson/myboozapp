0a94c4a924a8fe061f340e720da796485426d096
rapoulson <rebecca.poulson@gmail.com>
2014-10-09 07:44:05 -0400

    add use slugged to model

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -3,7 +3,7 @@
 class Drink < ActiveRecord::Base
   extend FriendlyId
 
-  friendly_id :name
+  friendly_id :name, use: :slugged
 
   has_many :drink_ingredients
   has_many :ingredients, :through => :drink_ingredients
