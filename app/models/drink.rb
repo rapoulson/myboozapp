ed4b7897282122d2a7e37d9492836c487b36b70f
rapoulson <rebecca.poulson@gmail.com>
2014-10-09 10:50:26 -0400

    name in url, but breaking view

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,10 +1,14 @@
 class Drink < ActiveRecord::Base
   validates_uniqueness_of :name
-  
+
   has_many :drink_ingredients
   has_many :ingredients, :through => :drink_ingredients
 
   def to_param
-    "#{id} #{name}".parameterize
+    "#{name}".parameterize
+  end
+
+  def self.find(input)
+    find_by_name(input)
   end
 end
