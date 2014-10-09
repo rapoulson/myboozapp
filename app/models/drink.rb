59e5c44c3775ff333847b2a6eeb0d2f39fc63f84
rapoulson <rebecca.poulson@gmail.com>
2014-10-08 21:57:57 -0400

    the ugliest of ways

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,4 +1,8 @@
 class Drink < ActiveRecord::Base
   has_many :drink_ingredients
   has_many :ingredients, :through => :drink_ingredients
+
+  def to_param
+    "#{id} #{name}".parameterize
+  end
 end
