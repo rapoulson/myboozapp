1967b7df170e2f032d5dc7069d11edc68cc18238
rapoulson <rebecca.poulson@gmail.com>
2014-10-08 22:38:16 -0400

    id no longer in url

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -69,7 +69,7 @@ class DrinksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_drink
-      @drink = Drink.find(params[:id])
+       @drink = Drink.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,8 +1,12 @@
+
+
 class Drink < ActiveRecord::Base
+  extend FriendlyId
+
+  friendly_id :name
+
   has_many :drink_ingredients
   has_many :ingredients, :through => :drink_ingredients
 
-  def to_param
-    "#{id} #{name}".parameterize
-  end
+
 end
