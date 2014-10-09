9bbb088bdc288336c74527f29dabcb3e6af32ff4
rapoulson <rebecca.poulson@gmail.com>
2014-10-09 13:55:36 -0400

    test still not configured right

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  require File.expand_path("../../config/environment", __FILE__)
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
